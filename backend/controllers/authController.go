f5876b75ad0b0fa0927068c8329ccef36aac0bbe
rondon23 <brunorondon21@gmail.com>
2022-08-26 13:05:04 -0400

    implementing request data

--- backend/controllers/authController.go
+++ backend/controllers/authController.go
@@ -6,11 +6,25 @@ import (
 )
 
 func Register(c *fiber.Ctx) error {
-	user := models.User{
-		FirstName: "John",
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
 	}
 
-	user.LastName = "Doe"
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "passwords do not match",
+		})
+	}
+
+	user := models.User{
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+		Password:  data["password"],
+	}
 
 	return c.JSON(user)
 }
--- backend/database/connect.go
+++ backend/database/connect.go
@@ -1,14 +1,17 @@
 package database
 
 import (
+	"github.com/rondon23/go-admin/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() {
-	_, err := gorm.Open(mysql.Open("developer:1234567@/go_admin"), &gorm.Config{})
+	database, err := gorm.Open(mysql.Open("developer:1234567@/go_admin"), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to the database")
 	}
+
+	database.AutoMigrate(&models.User{})
 }
