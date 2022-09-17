172f511b596f9e191dfed7b246c4bba3752d9df7
rondon23 <brunorondon21@gmail.com>
2022-09-17 19:24:36 -0400

    creating CRUD operation for user

--- backend/controllers/userController.go
+++ backend/controllers/userController.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/rondon23/go-admin/database"
 	"github.com/rondon23/go-admin/models"
@@ -26,4 +28,44 @@ func CreateUser(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+func GetUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	database.DB.Find(&user)
+
+	return c.JSON(user)
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+
+	database.DB.Model(&user).Updates(user)
+
+	return c.JSON(user)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	database.DB.Delete(&user)
+
+	return nil
 }
