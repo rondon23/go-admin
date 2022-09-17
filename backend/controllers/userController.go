c54df57ae6dea451fc1c8e84d4d61813ad2e8b9d
rondon23 <brunorondon21@gmail.com>
2022-09-16 23:17:01 -0400

    creating methods for add new user

--- backend/controllers/userController.go
+++ backend/controllers/userController.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/rondon23/go-admin/database"
 	"github.com/rondon23/go-admin/models"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func AllUsers(c *fiber.Ctx) error {
@@ -22,9 +21,7 @@ func CreateUser(c *fiber.Ctx) error {
 		return err
 	}
 
-	password, _ := bcrypt.GenerateFromPassword([]byte("1234"), 14)
-
-	user.Password = password
+	user.SetPassword("1234")
 
 	database.DB.Create(&user)
 
