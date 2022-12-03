9380de49b4f05406084e488761a54dfe0948b5c0
rondon23 <brunorondon21@gmail.com>
2022-12-03 08:24:40 -0400

    creating login in frontend

--- frontend/react-admin/src/App.tsx
+++ frontend/react-admin/src/App.tsx
@@ -7,6 +7,7 @@ import Dashboard from './pages/Dashboard';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Users from './pages/Users';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path={'/'} element={<Dashboard />} />
           <Route path={'/users'} element={<Users />} />
           <Route path={'/register'} element={<Register />} />
+          <Route path={'/login'} element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
