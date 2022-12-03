bc21bfbca92d2013452b10bc48be91f820556212
rondon23 <brunorondon21@gmail.com>
2022-12-02 21:27:32 -0400

    creating components

---
+++ frontend/react-admin/src/components/Menu.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu = () => {
+    return (
+        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <div className="position-sticky pt-3">
+                <ul className="nav flex-column">
+                    <li className="nav-item">
+                        <Link to={'/'} className="nav-link">
+                            <span data-feather="home"></span>
+                            Dashboard
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={'/users'} className="nav-link">
+                            <span data-feather="home"></span>
+                            Users
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Menu;
