a2571032ff4ec9dab8bb0c6c985ce76b80903261
rondon23 <brunorondon21@gmail.com>
2022-12-10 18:10:56 -0400

    creating models

--- frontend/react-admin/src/components/Nav.tsx
+++ frontend/react-admin/src/components/Nav.tsx
@@ -1,18 +1,22 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { User } from "../models/user";
 
 const Nav = () => {
-    const [user, setUser] = useState({
-        first_name: ''
-    });
+    const [user, setUser] = useState(new User());
 
     useEffect(() => {
         (
             async () => {
                 const { data } = await axios.get("user");
             
-                setUser(data);
+                setUser(new User(
+                    data.id,
+                    data.first_name,
+                    data.last_name,
+                    data.email,
+                ));
             }
         )();
 
@@ -28,7 +32,7 @@ const Nav = () => {
             <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company Name</a>
 
             <ul className="my-2 my-md-0 mr-md-3">
-                <Link to="/profile" className="p-2 text-white text-decoration-none">{user?.first_name}</Link>
+                <Link to="/profile" className="p-2 text-white text-decoration-none">{user.name}</Link>
                 <Link to="/login" className="p-2 text-white text-decoration-none" 
                     onClick={logout}
                 >Sign out</Link>
